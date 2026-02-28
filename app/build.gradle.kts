799c1270d20f413132186e892a32d8a7e11550e0
Vipula Liyanaarachchi <vipulasrilanka@yahoo.com>
2026-02-28 12:50:37 +0530

    Version 1.2 update, fixed warnings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Date
 import java.util.Locale
 import java.io.FileInputStream
 import java.util.Properties
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
@@ -55,8 +56,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
     buildFeatures {
         compose = true
